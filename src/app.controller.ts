d2740874b995ddda9492505409b458e5bfc41d50
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-01-27 20:50:48 -0300

    endpoint with queries

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,19 +9,24 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
   @Get('new')
   newEndpoint(): string {
     return this.appService.getNewEndpoint();
   }
+
   @Get('products/:productId')
   newEndpointWithParam(@Param('productId') productId: string): string {
     return this.appService.getEndpointWithParam(productId);
   }
+
   @Get('categories/:categoryId/products/:productId')
   getCategory(
     @Param('categoryId') categoryId: string,
     @Param('productId') productId: string,
+    @Query('offset') offset = 0,
+    @Query('limit') limit = 0,
   ): string {
-    return this.appService.getCategory(categoryId, productId);
+    return this.appService.getCategory(categoryId, productId, offset, limit);
   }
 }
